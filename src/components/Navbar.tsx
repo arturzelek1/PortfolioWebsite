1db9a75c3edee71895fe84f360cc3a37f1250691
Artur Zelek <1a.artur.zelek@gmail.com>
2025-03-18 20:36:20 +0100

    FrontPage
    +Navbar
    +FrontPage

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,55 @@
+import "../styles/Navbar.scss";
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+      <div className="container-fluid">
+        <a className="navbar-brand">
+          <img
+            src="/assets/avatarFigma.JPG"
+            className="Avatar me-3 ms-3"
+            alt="Picture of Developer"
+          />
+          ARTUR ZELEK
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav justify-content-end ms-auto">
+            <li className="nav-item">
+              <a className="nav-link mx-3" href="/Home">
+                HOME
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link mx-3" href="/About">
+                ABOUT
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link mx-3" href="/Projects">
+                PROJECTS
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link mx-3" href="/Contact">
+                CONTACT
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
